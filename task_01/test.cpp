df9dd4631c9135fcb7bd39a608cd961aa542399d
Ksupall <KsenyaPolyakovaaa@mail.ru>
2019-10-18 10:31:48 +0300

    add first test for Gtests

--- task_01/test.cpp
+++ task_01/test.cpp
@@ -1,4 +1,11 @@
 #include "gtest/gtest.h"
+#include "main.h"
+
+TEST(check_data, wrong_year)
+{
+	ASSERT_EQ(WRONG_YEAR, check_data(3, 11, -100));
+	ASSERT_EQ(WRONG_YEAR, check_data(3, 11, 1980));
+}
 
 int main(int argc, char **argv)
 {
