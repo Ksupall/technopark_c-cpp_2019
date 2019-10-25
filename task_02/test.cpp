9d5604e8763ddf90b6eb41416d9a0784e705d8f8
Ksupall <KsenyaPolyakovaaa@mail.ru>
2019-10-26 01:36:18 +0300

    add second test

--- task_02/test.cpp
+++ task_02/test.cpp
@@ -4,26 +4,19 @@
 #include "err_codes.h"
 #include <time.h>
 
-int create_string(char *string, int len) {
-  srand(time(NULL));
-  char symb[52] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
-                  'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 
-                  'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W',
-                  'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S',
-                  'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C',
-                  'V', 'B', 'N', 'M'};
-  for (int i = 0; i < len; i++) {
-      int a = rand() % 52;
-      string[i] = symb[a];
-  }
-  return 0;
-}
-
-TEST(comare_libs, len_is_null)
+TEST(comare_libs, empty_file)
 {
   char str[] = {'y', 'e', 's'};
   int res_ser = serial("data/in_0.txt", str, 5, 3); 
   int res_parall = parallel("data/in_0.txt", str, 5, 3);
+  ASSERT_EQ(res_ser, res_parall);
+}
+
+TEST(comare_libs, elements_100)
+{
+  char str[] = {'k', 'c', 'a'};
+  int res_ser = serial("data/in_1.txt", str, 100, 3); 
+  int res_parall = parallel("data/in_0.txt", str, 100, 3);
   ASSERT_EQ(res_ser, res_parall);
 }
 
