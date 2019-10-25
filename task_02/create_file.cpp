ed6007e04f43f437829a4fcdb4eba7e5fb761aa3
Ksupall <KsenyaPolyakovaaa@mail.ru>
2019-10-25 12:41:22 +0300

    add source code for task_02

--- task_02/create_file.cpp
+++ task_02/create_file.cpp
@@ -4,6 +4,10 @@
 
 int main () {
 	srand(time(NULL));
+	char four_symb[20][5] = {"tree", "foot", "door", "bear", "vibe",
+							"neck", "rose", "week", "bike", "beer",
+							"road", "year", "kite", "wine", "fork",
+							"cock", "page", "sign", "leaf", "wind"};
 	char symb[52] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
 				'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 
 				'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W',
@@ -11,13 +15,13 @@ int main () {
 				'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C',
 				'V', 'B', 'N', 'M'};
 	FILE *f;
-	f = fopen("in_1.txt", "w");
+	f = fopen("in_2.txt", "w");
 	if (!f)
 		printf("Couldn't find file!\n");
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 25; i++)
 	{
-		int a = rand() % 52;
-		fprintf(f, "%c", symb[a]);
+		int a = rand() % 20;
+		fprintf(f, "%s", four_symb[a]);
 	}
 	fclose(f);
 	
