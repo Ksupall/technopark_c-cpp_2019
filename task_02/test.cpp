2a8df5cdebed1e8784136ef769e0e4b82a18d9b9
Ksupall <KsenyaPolyakovaaa@mail.ru>
2019-10-26 07:29:42 +0300

    add some tests

--- task_02/test.cpp
+++ task_02/test.cpp
@@ -20,6 +20,22 @@ TEST(comare_libs, subst_3_mainstr_100)
   ASSERT_EQ(res_ser, res_parall);
 }
 
+TEST(comare_libs, subst_2_mainstr_100)
+{
+  char str[] = {'j', 't'};
+  int res_ser = serial("data/in_1.txt", str, 100, 2); 
+  int res_parall = parallel("data/in_1.txt", str, 100, 2);
+  ASSERT_EQ(res_ser, res_parall);
+}
+
+TEST(comare_libs, subst_1_mainstr_100)
+{
+  char str[] = {'j'};
+  int res_ser = serial("data/in_1.txt", str, 100, 3); 
+  int res_parall = parallel("data/in_1.txt", str, 100, 3);
+  ASSERT_EQ(res_ser, res_parall);
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
