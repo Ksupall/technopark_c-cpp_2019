471db2c2b67993790f2ee04478878c9f7c574266
Ksupall <KsenyaPolyakovaaa@mail.ru>
2019-10-26 03:02:37 +0300

    changed test

--- task_02/test.cpp
+++ task_02/test.cpp
@@ -12,14 +12,6 @@ TEST(comare_libs, empty_file)
   ASSERT_EQ(res_ser, res_parall);
 }
 
-TEST(comare_libs, elements_100_str_3)
-{
-  char str[] = {'k', 'c', 'a'};
-  int res_ser = serial("data/in_1.txt", str, 100, 3); 
-  int res_parall = parallel("data/in_0.txt", str, 100, 3);
-  ASSERT_EQ(res_ser, res_parall);
-}
-
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
