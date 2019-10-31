76ceefaf2c811bbf64a3c3169c3f731b2e6065ca
Ksupall <KsenyaPolyakovaaa@mail.ru>
2019-10-31 12:13:21 +0300

    changed tests

--- task_02/test.cpp
+++ task_02/test.cpp
@@ -16,7 +16,7 @@ TEST(comare_libs, subst_11_mainstr_100)
 {
   char str[] = {'S', 'g', 'W', 't', 'X', 'd', 'd', 'k', 'j', 'p', 'u'};
   int res_ser = serial("data/in_1.txt", str, 100, 11); 
-  int res_parall = parallel("data/in_1.txt", str, 100, 11);
+  int res_parall = parallel("data/in_1.txt", str, 100, 11, 4);
   ASSERT_EQ(res_ser, res_parall);
 }
 
@@ -24,7 +24,7 @@ TEST(comare_libs, subst_10_mainstr_100_have)
 {
   char str[] = {'j', 't', 'k', 'm', 'o', 'y', 'm', 'P', 'L', 'e'};
   int res_ser = serial("data/in_1.txt", str, 100, 10); 
-  int res_parall = parallel("data/in_1.txt", str, 100, 10);
+  int res_parall = parallel("data/in_1.txt", str, 100, 10, 4);
   ASSERT_EQ(res_ser, res_parall);
 }
 
@@ -32,7 +32,7 @@ TEST(comare_libs, subst_10_mainstr_100_notHave)
 {
   char str[] = {'j', 't', 'k', 'l', 'l', 'l', 'l', 'P', 'L', 'e'};
   int res_ser = serial("data/in_1.txt", str, 100, 10); 
-  int res_parall = parallel("data/in_1.txt", str, 100, 10);
+  int res_parall = parallel("data/in_1.txt", str, 100, 10, 4);
   ASSERT_EQ(res_ser, res_parall);
 }
 
@@ -40,7 +40,7 @@ TEST(comare_libs, subst_3_mainstr_100)
 {
   char str[] = {'j', 't', 'k'};
   int res_ser = serial("data/in_1.txt", str, 100, 3); 
-  int res_parall = parallel("data/in_1.txt", str, 100, 3);
+  int res_parall = parallel("data/in_1.txt", str, 100, 3, 4);
   ASSERT_EQ(res_ser, res_parall);
 }
 
@@ -48,7 +48,7 @@ TEST(comare_libs, subst_2_mainstr_100)
 {
   char str[] = {'j', 't'};
   int res_ser = serial("data/in_1.txt", str, 100, 2); 
-  int res_parall = parallel("data/in_1.txt", str, 100, 2);
+  int res_parall = parallel("data/in_1.txt", str, 100, 2, 4);
   ASSERT_EQ(res_ser, res_parall);
 }
 
@@ -56,7 +56,7 @@ TEST(comare_libs, subst_1_mainstr_100)
 {
   char str[] = {'j'};
   int res_ser = serial("data/in_1.txt", str, 100, 1); 
-  int res_parall = parallel("data/in_1.txt", str, 100, 1);
+  int res_parall = parallel("data/in_1.txt", str, 100, 1, 4);
   ASSERT_EQ(res_ser, res_parall);
 }
 
